13dff75b04bdcee3d350baed0e773b1a3184fc31
Daria Steblovska <darias1896.96@gmail.com>
2026-01-10 23:07:53 +0000

    task 3 added

--- lesson-3/task-3/routes/index.mjs
+++ lesson-3/task-3/routes/index.mjs
@@ -10,13 +10,22 @@ router.get('/goals', (req, res) => {
     res.render('goals', { title: 'Goals', goals: goals })
 })
 
-router.get('/info/:myLink', (req, res) => {
+router.get('/info/:myLink?', (req, res) => {
     const { myLink } = req.params
 
     let info = null
     if (myLink === 'sites') info = sites
-    if (myLink === 'films') info = films
-    if (myLink === 'me') info = [{ title: 'My website', description: 'This is info about me' }]
+    else if (myLink === 'films') info = films
+    else if (myLink === 'me') info = [{ title: 'My website', description: 'This is info about me' }]
+    else {
+        const error = {
+            message: 'Error. Invalid parameter.', status: '400', stack: ''
+        }
+        return res.status(400).render('error', {
+            message: error.message,
+            error: error
+        })
+    }
 
     res.render('info', { title: myLink, info })
 })
