2c55141fef8cb2adf7331794bc121c6c383d33ec
Daria Steblovska <darias1896.96@gmail.com>
2026-01-23 02:45:22 +0000

    controllers updated with update book func

--- lesson-4/controllers/bookController.mjs
+++ lesson-4/controllers/bookController.mjs
@@ -2,30 +2,70 @@ import Book from "../models/bookModel.mjs";
 
 class BookController {
     static getAllBooks(req, res) {
-        const booksList = Book.loadBooksList()
-        res.render('books/booksList', {
-            books: booksList,
-        })
+        try {
+            const booksList = Book.loadBooksList()
+            res.render('books/booksList', {
+                title: 'Books List',
+                books: booksList,
+            })
+        } catch (error) {
+            res.status(500).render('error', {
+                message: 'Error loading books',
+                error
+            })
+        }
     }
     static getBookById(req, res) {
-        const id = req.params.id
-        const book = Book.getBookById(id)
-        res.render('books/bookDetail', {
-            book,
-        })
+        try {
+            const id = req.params.id
+            const book = Book.getBookById(id)
+            res.render('books/bookDetail', {
+                title: 'Books Detail',
+                book,
+            })
+        } catch (error) {
+            res.status(500).render('error', {
+                message: 'Error loading book',
+                error
+            })
+        }
     }
     static updateBook(req, res) {
-       
+        try {
+            const id = req.params
+            const bookData = req.body
+            Book.updateBook(id, bookData)
+            res.redirect('/books')
+        } catch (error) {
+            res.status(500).render('error', {
+                message: 'Error updating book',
+                error
+            })
+        }
     }
     static createBook(req, res) {
-        const bookData = req.body
-        Book.addNewBook(bookData)
-        res.redirect('/books')
+        try {
+            const bookData = req.body
+            Book.addNewBook(bookData)
+            res.redirect('/books')
+        } catch (error) {
+            res.status(500).render('error', {
+                message: 'Error creating book',
+                error
+            })
+        }
     }
     static deleteeBook(req, res) {
-        const id = req.params.id
-        Book.deleteBookById(id)
-        res.status(204).end()
+        try {
+            const id = req.params.id
+            Book.deleteBookById(id)
+            res.status(204).end()
+        } catch (error) {
+            res.status(500).render('error', {
+                message: 'Error deleting book',
+                error
+            })
+        }
     }
 }
 
