47ab065e059a9493ed9dd35aaa186f847ab42838
Daria Steblovska <darias1896.96@gmail.com>
2026-02-04 00:58:20 +0000

    delete car on client side and changing img handler added

--- lesson-5/public/javascripts/script.js
+++ lesson-5/public/javascripts/script.js
@@ -1,17 +1,29 @@
-// async function deleteBook(id) {
-//     try {
-//         const res = await fetch('/books', {
-//             method: 'DELETE',
-//             headers: {'content-type': 'application/json'},
-//             body: JSON.stringify({id})
-//         })
-//         console.log(res.status)
-//         if(res.ok) {
-//             // location.reload()
-//             window.location.href = '/books'
-//         }
-//     } catch (error) {
-//         console.log(error);
+async function deleteCar(id) {
+    try {
+        const res = await fetch('/cars', {
+            method: 'DELETE',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({id})
+        })
+        // console.log(res.status)
+        if(res.ok) {
+            // location.reload()
+            window.location.href = '/cars'
+        }
+    } catch (error) {
+        console.log(error);
         
-//     }
-// }
+    }
+}
+
+function handleImgInput(event) {
+    const file = event.target.files[0]
+    if(file) {
+        const imgEl = document.getElementById('carImg')
+        const reader = new FileReader()
+        reader.onload = function(e) {
+            imgEl.src = e.target.result
+        }
+        reader.readAsDataURL(file)
+    }
+}
