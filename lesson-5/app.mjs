d4d2f865379a35d0bc8df99e909428e43e069d2d
Daria Steblovska <darias1896.96@gmail.com>
2026-02-04 00:55:14 +0000

    imports updated

--- lesson-5/app.mjs
+++ lesson-5/app.mjs
@@ -1,22 +1,19 @@
 import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
-// import logger from 'morgan'
-import { fileURLToPath } from 'url'
 import indexRouter from './routes/index.mjs'
+import { __dirname, __filename } from "./settings.mjs";
+
 const app = express()
-const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
-const __dirname = path.dirname(__filename) // get the name of the directory
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', indexRouter)
-// catch 404 and forward to error handler
+app.use(express.static(path.join(__dirname, 'uploads')))
+app.use('/', indexRouter) // catch 404 and forward to error handler
 app.use((req, res, next) => {
 const err = new Error('Not Found')
 err.status = 404
