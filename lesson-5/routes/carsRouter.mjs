6585231b4282975f68fdd7042af11485d1234922
Daria Steblovska <darias1896.96@gmail.com>
2026-02-04 00:57:33 +0000

    import updated and middleware added to uplod images

--- lesson-5/routes/carsRouter.mjs
+++ lesson-5/routes/carsRouter.mjs
@@ -1,16 +1,18 @@
 import { Router } from 'express'
 import CarController from "../controllers/carController.mjs";
+import uploadImg from "../middleware/uploadImg.mjs";
 
 const router = Router()
 
 router.get('/cars', CarController.getAllCars)
 router.get('/car/:id', CarController.getCarById)
 
-router.post('/update-car/:id', CarController.updateCar)
+router.post('/update-car/:id',
+uploadImg.array('images', 10), CarController.updateCar)
 router.get('/update-car/:id', CarController.getCarForm)
 
 router.get('/create-car', CarController.getCarForm)
-router.post('/create-car', CarController.createCar)
+router.post('/create-car', uploadImg.array('images', 10), CarController.createCar)
 
 router.delete('/cars', CarController.deleteCar)
 
