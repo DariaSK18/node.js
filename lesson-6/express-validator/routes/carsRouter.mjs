d467b981308a1644ce1542bf84684f4b66c5d367
Daria Steblovska <darias1896.96@gmail.com>
2026-02-10 05:37:08 +0000

    validation schema and chain connected in routers

--- lesson-6/express-validator/routes/carsRouter.mjs
+++ lesson-6/express-validator/routes/carsRouter.mjs
@@ -1,18 +1,30 @@
 import { Router } from 'express'
 import CarController from "../controllers/carController.mjs";
 import uploadImg from "../middleware/uploadImg.mjs";
+import FormValidator from "../utils/validationSchema.js";
+import { checkSchema } from 'express-validator';
 
 const router = Router()
 
-router.get('/cars', CarController.getAllCars)
+router.get('/cars', FormValidator.filterValidationChain, CarController.getAllCars)
 router.get('/car/:id', CarController.getCarById)
 
-router.post('/update-car/:id',
-uploadImg.array('images', 10), CarController.updateCar)
+// --- chain validator ---
+router.post('/update-car/:id', uploadImg.array('images', 10), FormValidator.formValidationChain, CarController.updateCar)
+
+// --- schema validation ---
+// router.post('/update-car/:id',
+// uploadImg.array('images', 10), checkSchema(FormValidator.formValidationSchema), CarController.updateCar)
+
 router.get('/update-car/:id', CarController.getCarForm)
 
 router.get('/create-car', CarController.getCarForm)
-router.post('/create-car', uploadImg.array('images', 10), CarController.createCar)
+
+// --- chain validator ---
+router.post('/create-car', uploadImg.array('images', 10), FormValidator.formValidationChain, CarController.createCar)
+
+// --- schema validation ---
+// router.post('/create-car', uploadImg.array('images', 10), checkSchema(FormValidator.formValidationSchema), CarController.createCar)
 
 router.delete('/cars', CarController.deleteCar)
 
