6c0202756dba806704096ea1606c6b03ea224608
Daria Steblovska <darias1896.96@gmail.com>
2026-02-11 08:22:17 +0000

    routes updated using middleware

--- lesson-6/zod/routes/carsRouter.mjs
+++ lesson-6/zod/routes/carsRouter.mjs
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import CarController from "../controllers/carController.mjs";
 import uploadImg from "../middleware/uploadImg.mjs";
+import { formValidationMiddleware } from "../middleware/validationMiddleware.js";
+import { formValidationSchema } from "../utils/validationSchema.js";
 
 const router = Router()
 
@@ -8,11 +10,12 @@ router.get('/cars', CarController.getAllCars)
 router.get('/car/:id', CarController.getCarById)
 
 router.post('/update-car/:id',
-uploadImg.array('images', 10), CarController.updateCar)
+uploadImg.array('images', 10), 
+formValidationMiddleware(formValidationSchema), CarController.updateCar)
 router.get('/update-car/:id', CarController.getCarForm)
 
 router.get('/create-car', CarController.getCarForm)
-router.post('/create-car', uploadImg.array('images', 10), CarController.createCar)
+router.post('/create-car', uploadImg.array('images', 10), formValidationMiddleware(formValidationSchema), CarController.createCar)
 
 router.delete('/cars', CarController.deleteCar)
 
