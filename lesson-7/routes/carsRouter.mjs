1f4c4ccf36219119ea770033c024a2eeffcc4a91
Daria Steblovska <darias1896.96@gmail.com>
2026-02-26 21:15:26 +0000

    car routes updated using zod validation

--- lesson-7/routes/carsRouter.mjs
+++ lesson-7/routes/carsRouter.mjs
@@ -1,30 +1,21 @@
 import { Router } from 'express'
 import CarController from "../controllers/carController.mjs";
 import uploadImg from "../middleware/uploadImg.mjs";
-import FormValidator from "../utils/validationSchema.js";
-import { checkSchema } from 'express-validator';
+import { filterValidationMiddleware, formValidationMiddleware } from "../middleware/validationMiddleware.js";
+import { filterValidationSchema, formValidationSchema } from "../validation/validationSchema.js";
 
 const router = Router()
 
-router.get('/cars', FormValidator.filterValidationChain, CarController.getAllCars)
+router.get('/cars', filterValidationMiddleware(filterValidationSchema), CarController.getAllCars)
 router.get('/car/:id', CarController.getCarById)
 
-// --- chain validator ---
-router.post('/update-car/:id', uploadImg.array('images', 10), FormValidator.formValidationChain, CarController.updateCar)
-
-// --- schema validation ---
-// router.post('/update-car/:id',
-// uploadImg.array('images', 10), checkSchema(FormValidator.formValidationSchema), CarController.updateCar)
-
+router.post('/update-car/:id',
+uploadImg.array('images', 10), 
+formValidationMiddleware(formValidationSchema), CarController.updateCar)
 router.get('/update-car/:id', CarController.getCarForm)
 
 router.get('/create-car', CarController.getCarForm)
-
-// --- chain validator ---
-router.post('/create-car', uploadImg.array('images', 10), FormValidator.formValidationChain, CarController.createCar)
-
-// --- schema validation ---
-// router.post('/create-car', uploadImg.array('images', 10), checkSchema(FormValidator.formValidationSchema), CarController.createCar)
+router.post('/create-car', uploadImg.array('images', 10), formValidationMiddleware(formValidationSchema), CarController.createCar)
 
 router.delete('/cars', CarController.deleteCar)
 
